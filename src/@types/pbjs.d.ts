e62e8f1136c619813d8b0c5bd992830ba1f4b5e9
Salvatore Zappalà <salvatore@salvatorezappala.info>
2020-12-17 21:59:16 +0100

    Add pbjs types

--- src/@types/pbjs.d.ts
+++ src/@types/pbjs.d.ts
@@ -1,1 +1,25 @@
-declare const pbjs: any
+interface Bid {
+    adUnitCode: string;
+    adId: string;
+    bidder: string;
+}
+
+interface RespondedBid extends Bid {
+    timeToRespond: number;
+    cpm: number;
+    statusMessage: string;
+}
+
+interface BidsByAdUnit {
+    [adUnitCode: string]: Bid
+}
+
+interface RespondedBidsByAdUnit {
+    [adUnitCode: string]: RespondedBid
+}
+
+declare namespace pbjs {
+    function getAllWinningBids(): RespondedBid[]; // TODO verify return type by testing
+    function getBidResponses(): BidsByAdUnit;
+    function getNoBids(): RespondedBidsByAdUnit;
+}
