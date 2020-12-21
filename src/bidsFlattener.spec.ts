51d4ea377e9648aa479c261d6dce2eaf176e4753
Salvatore Zappalà <salvatore@salvatorezappala.info>
2020-12-21 19:05:40 +0100

    Test: should return multiple bids from multiple keys

--- src/bidsFlattener.spec.ts
+++ src/bidsFlattener.spec.ts
@@ -36,19 +36,31 @@ describe("flattenBids()", () => {
     it("should return multiple bids from multiple keys", () => {
         const bids: BidsByAdUnit = {
             "x": {
-                bids: [{
-                    adId: 'adId',
-                    adUnitCode: 'adUnitCode',
-                    bidder: 'bidder'
-                }]
+                bids: [
+                    {
+                        adId: 'adId',
+                        adUnitCode: 'x',
+                        bidder: 'bidder'
+                    }
+                ]
+            },
+            "y": {
+                bids: [
+                    {
+                        adId: 'adId2',
+                        adUnitCode: 'y',
+                        bidder: 'bidder2'
+                    },
+                    {
+                        adId: 'adId3',
+                        adUnitCode: 'y',
+                        bidder: 'bidder3'
+                    }
+                ]
             }
         };
         const flattenedBids = flattenBids(bids);
-        expect(flattenedBids[0]).toEqual({
-            adId: 'adId',
-            adUnitCode: 'adUnitCode',
-            bidder: 'bidder'
-        });
+        expect(flattenedBids.length).toBe(3);
     });
 
     // it should return more than one bid
