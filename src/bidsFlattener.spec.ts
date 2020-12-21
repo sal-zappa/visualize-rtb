e5a8d85bfa45bb844c219e44581d5f63ffaa7e09
Salvatore Zappalà <salvatore@salvatorezappala.info>
2020-12-21 18:51:12 +0100

    Test that bid is flattened

--- src/bidsFlattener.spec.ts
+++ src/bidsFlattener.spec.ts
@@ -12,4 +12,20 @@ describe("flattenBids()", () => {
         const flattenedBids = flattenBids(bids);
         expect(flattenedBids.length).toBe(1);
     });
+
+    it("should return the flattened bid", () => {
+        const bids: BidsByAdUnit = {
+            'x': {
+                adId: 'adId',
+                adUnitCode: 'adUnitCode',
+                bidder: 'bidder'
+            }
+        };
+        const flattenedBids = flattenBids(bids);
+        expect(flattenedBids[0]).toEqual({
+            adId: 'adId',
+            adUnitCode: 'adUnitCode',
+            bidder: 'bidder'
+        });
+    });
 });
