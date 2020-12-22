81e6d3ba75566bc603f9981f0807f2aa7bd2e3d5
Salvatore Zappalà <salvatore@salvatorezappala.info>
2020-12-22 22:46:12 +0100

    Add TODO

--- src/bidsFetcher.ts
+++ src/bidsFetcher.ts
@@ -1,7 +1,7 @@
 import { flattenBids } from "./bidsFlattener";
 
 export function fetchAllBids() {
-    const winningBids = pbjs.getAllWinningBids();
+    const winningBids = pbjs.getAllWinningBids(); // TODO pbjs should be injected
     const respondedBids = flattenBids(pbjs.getBidResponses());
     const unrespondedBids = flattenBids(pbjs.getNoBids());
 
