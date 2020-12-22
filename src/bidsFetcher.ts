43c887c2cef2a7c31b196a91c03b4976f43cd948
Salvatore Zappalà <salvatore@salvatorezappala.info>
2020-12-22 22:30:28 +0100

    Add bidsFetcher

---
+++ src/bidsFetcher.ts
@@ -0,0 +1,11 @@
+import { flattenBids } from "./bidsFlattener";
+
+const winningBids = pbjs.getAllWinningBids();
+
+const bidResponsesByAdUnit = pbjs.getBidResponses();
+const respondedBids = flattenBids(bidResponsesByAdUnit) as RespondedBid[];
+
+const noBidsByAdUnit = pbjs.getNoBids();
+const unrespondedBids = flattenBids(noBidsByAdUnit);
+
+const allBids = [...winningBids, ...respondedBids, ...unrespondedBids];
