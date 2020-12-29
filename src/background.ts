df20a47ddc02eb914198ea1771d6771ee289215a
Salvatore Zappalà <salvatore@salvatorezappala.info>
2020-12-29 20:47:16 +0100

    Background color red on click:

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,12 @@
 import { fetchAllBids } from "./bidsFetcher";
 
-console.log(fetchAllBids());
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // if (typeof pbjs !== "undefined") {
+    //     console.log(fetchAllBids(pbjs));
+    // }
+
+    chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor="red"'
+    });
+});
--- src/bidsFetcher.ts
+++ src/bidsFetcher.ts
@@ -1,12 +1,12 @@
 import { flattenBids } from "./bidsFlattener";
 
-export function fetchAllBids() {
-    const winningBids = pbjs.getAllWinningBids(); // TODO pbjs should be injected
+export function fetchAllBids(p: typeof pbjs) {
+    const winningBids = p.getAllWinningBids(); // TODO pbjs should be injected
     for (const winningBid of winningBids) {
         winningBid.winner = true;
     }
-    const respondedBids = flattenBids(pbjs.getBidResponses());
-    const unrespondedBids = flattenBids(pbjs.getNoBids());
+    const respondedBids = flattenBids(p.getBidResponses());
+    const unrespondedBids = flattenBids(p.getNoBids());
 
     return [...winningBids, ...respondedBids, ...unrespondedBids];
 }
