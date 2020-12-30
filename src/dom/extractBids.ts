974ca0df07ae56c9b87d6f9e5da42e8cd5978200
Salvatore Zappalà <salvatore@salvatorezappala.info>
2020-12-30 18:27:15 +0100

    Add function to filter out the attributes we don't need

--- src/dom/extractBids.ts
+++ src/dom/extractBids.ts
@@ -12,3 +12,17 @@ function fetchAllBids(library: typeof pbjs) {
 function flattenBids(bidsByAdUnit: BidsByAdUnit): Bid[] {
     return Object.values(bidsByAdUnit).map(x => x.bids).flat();
 }
+
+function filterAttributes(bids: Bid[]): Bid[] {
+    return bids.map((bid) => {
+        return {
+            adUnitCode: bid.adUnitCode,
+            adId: bid.adId,
+            bidder: bid.bidder,
+            timeToRespond: bid.timeToRespond,
+            cpm: bid.cpm,
+            statusMessage: bid.statusMessage,
+            winner: bid.winner
+        }
+    })
+}
