2506f307681c4026547ebcb5dc3c1bc797aa8729
Salvatore Zappalà <salvatore@salvatorezappala.info>
2021-03-24 21:11:52 +0100

    Integrate domscript in execute script

--- src/execute.js
+++ src/execute.js
@@ -2,32 +2,28 @@
     This script is executed when the extension icon is clicked
 */
 const domScript = `
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
-function fetchAllBids(library) {
-    var winningBids = library.getAllWinningBids();
-    for (var _i = 0, winningBids_1 = winningBids; _i < winningBids_1.length; _i++) {
-        var winningBid = winningBids_1[_i];
+function fetchAllBids(pbjs) {
+    const winningBids = pbjs.getAllWinningBids();
+    for (const winningBid of winningBids) {
         winningBid.winner = true;
     }
-    var respondedBids = flattenBids(library.getBidResponses());
-    var unrespondedBids = flattenBids(library.getNoBids());
-    return __spreadArrays(winningBids, respondedBids, unrespondedBids);
+    const respondedBids = flattenBids(pbjs.getBidResponses());
+    const unrespondedBids = flattenBids(pbjs.getNoBids());
+
+    return [...winningBids, ...respondedBids, ...unrespondedBids];
 }
+
 function flattenBids(bidsByAdUnit) {
-    return Object.values(bidsByAdUnit).map(function (x) { return x.bids; }).flat();
+    return Object.values(bidsByAdUnit).map(x => x.bids).flat();
 }
+
 if (typeof pbjs !== "undefined") {
-    var allBids = fetchAllBids(pbjs);
-    var dataElement = document.createElement("div");
+    const allBids = fetchAllBids(pbjs);
+    const dataElement = document.createElement("div");
     dataElement.id = "visualise-rtb-data";
     dataElement.setAttribute("data-bids", JSON.stringify(allBids));
     document.body.appendChild(dataElement);
+    console.log("data appended");
 }
 `;
 
